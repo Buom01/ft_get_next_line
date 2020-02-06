f4da08efe7a0ac12e5d6005e2333627dbfd2faca
Bastien ADAM <badam@e1r3p5.42.fr>
2020-02-07 00:28:46 +0100

    Add missing imports

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/04 17:06:14 by badam             #+#    #+#             */
-/*   Updated: 2020/02/05 02:01:33 by badam            ###   ########.fr       */
+/*   Updated: 2020/02/07 00:28:28 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define GET_NEXT_LINE_BONUS_H
 
 # include <stddef.h>
+# include <unistd.h>
 
 size_t	ft_strlen(const char *str);
 int		get_next_line(int fd, char **line);
