6ae132292c0b935ddeb4c3441de8c58ecee1139e
Bastien ADAM <badam@e1r3p5.42.fr>
2020-02-07 00:33:10 +0100

    Imports entire lib instead of definitions

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,14 +6,14 @@
 /*   By: badam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/04 17:06:14 by badam             #+#    #+#             */
-/*   Updated: 2020/02/07 00:28:28 by badam            ###   ########.fr       */
+/*   Updated: 2020/02/07 00:32:45 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-# include <stddef.h>
+# include <stdlib.h>
 # include <unistd.h>
 
 size_t	ft_strlen(const char *str);
